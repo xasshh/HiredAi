3fc538070a38c644364cc0b517208968386dfaf0
meshack <adeoyemeshack37@gmail.com>
2025-03-20 11:28:06 +0100

    google auth

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -19,27 +19,25 @@ class GoogleController extends Controller
 
     public function googlecallback()
     {
-        
-            $user = Socialite::driver('google')->user();
-
-            // Check if user exists
-            $existingUser = User::where('google_id', $user->id)->first();
-            
-            if (!$existingUser) {
-                // If user does not exist, create a new one
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'google_id' => $user->id, // Store Google ID as a string, not JSON
-                    'password' => bcrypt(Str::random(16)), // Generate a random password
-                ]);
-            
-                Auth::login($newUser);
-            } else {
-                Auth::login($existingUser);
+        $user = Socialite::driver('google')->user();
+    
+        $existingUser = User::where('email', $user->email)->first();
+        if (!$existingUser) {
+            User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id' => $user->id,
+                'password' => bcrypt(Str::random(16)),
+            ]);
+        } else {
+            // Update Google ID if it's missing
+            if (!$existingUser->google_id) {
+                $existingUser->update(['google_id' => $user->id]);
             }
-            
-            return redirect('/dashboard'); // Redirect user after login
-                  
+        }
+        Auth::login($existingUser);
+        return redirect('/dashboard');
+        
     }
+    
 }
